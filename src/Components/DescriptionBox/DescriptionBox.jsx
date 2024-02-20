fdcbb93cb1d33581ec76556fca12d4bcdaacd61d
aditiravindranayak <aditinayak1512@gmail.com>
2024-02-21 00:59:28 +0530

    Hello

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+export const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+        </div>
+        <div className="descriptionbox-descrption">
+          <p>An e-commerce website is an online platform that facilitates the buying and selling of products or services over the internet. It serves as a virtual marketplace where businesses and individuals can showcase their products, interact with
+customers, and conduct transactions without the need for a physical presence. E-cammerce websites have gained immense popularity due to their convenience, accessibility, and the global reach they offer.
+E-commerce websites typically display products or services along with detailed descriptions, images, prices, and any available variations (e.g., sizes, colors). Each product usually has its own dedicated page with relevant information.
+            </p>  
+        </div>
+    </div>
+  )
+}
+export default DescriptionBox
---
+++ src/Pages/Cart.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import CartItems from '../Components/CartItems/CartItems';
+
+const Cart = () => {
+  return (
+    <div>
+      <h1>Shopping Cart</h1>
+      <CartItems />
+    </div>
+  );
+};
+
+export default Cart;
